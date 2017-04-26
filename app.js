75288b30d0e7ae0c4042e8a876857e33f62eb724
gscharly <charlygs94@gmail.com>
2017-04-26 03:56:06 -0700

    Port updated

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.listen(8080, function () {
-  console.log('Listening on port 8080')
-})
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
