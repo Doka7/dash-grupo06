b2cc8695fd4e2dd0f362d8d063f2a93c0520ae2b
gscharly <charlygs94@gmail.com>
2017-05-08 12:41:46 +0200

    Metrics v2

--- public/javascripts/stats.js
+++ public/javascripts/stats.js
@@ -1,10 +1,16 @@
 var updateInterval = 1000;
 var player = dashjs.MediaPlayer().create();
+var url = "/clip_monza_manifest.mpd";
+video = document.querySelector(".dash-video-player");
+player.initialize(video, url, true);
 player.getDebug().setLogToBrowserConsole(false);
 var metricsInterval = null;
 
+//Metrics
+var videoBufferLevel=0;
+
 player.on(dashjs.MediaPlayer.events.STREAM_INITIALIZED, function(e){
-	console.log(e.type);
+	console.log("Event: "+e.type);
 	clearInterval(metricsInterval);
 	metricsInterval = setInterval(function(){
 		updateMetrics("video");
@@ -13,12 +19,19 @@ player.on(dashjs.MediaPlayer.events.STREAM_INITIALIZED, function(e){
 
 });
 
+player.on(dashjs.MediaPlayer.events.PLAYBACK_ENDED, function(e){
+	console.log("Event: "+e.type);
+	clearInterval(metricsInterval);
+
+});
+
 function updateMetrics(type){
 	var metrics = player.getMetricsFor(type);
 	var dashMetrics = player.getDashMetrics();
 	if (metrics && dashMetrics){
-		var bufferLevel = dashMetrics.getCurrentBufferLevel(metrics);
-		$("#buffer").text("Buffer length: " + bufferLevel);
+		videoBufferLevel = dashMetrics.getCurrentBufferLevel(metrics);
+		$("#buffer").text("Buffer length: " + videoBufferLevel);
+		console.log("Video buffer level: " +videoBufferLevel);
 	}
 };
 
